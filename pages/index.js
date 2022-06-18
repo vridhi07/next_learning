352a7f97d70ec60b3c7ddde27a343cac3c3fabb8
vridhi07 <vridhiw@gmail.com>
2022-06-19 02:42:11 +0530

    link component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,41 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
-export default function Home() {
-  return <div className={styles.container}>llllllllll</div>;
+export default function Home({ productId = 100 }) {
+  return (
+    <div>
+      <div className={styles.container}>Home Page</div>
+      <div>
+        {" "}
+        <Link href="/blog">
+          <a>Blog</a>
+        </Link>
+      </div>
+      <div>
+        {" "}
+        <Link href="/product/1">
+          <a>Product1</a>
+        </Link>
+      </div>{" "}
+      <div>
+        <Link href="/product/2">
+          <a>Product2</a>
+        </Link>
+      </div>{" "}
+      <div>
+        {" "}
+        <Link href="/product/3" replace>
+          <a>Product3</a>
+        </Link>
+      </div>{" "}
+      <div>
+        {" "}
+        <Link href={`/product/${productId}`}>
+          <a>Product {productId}</a>
+        </Link>
+      </div>{" "}
+    </div>
+  );
 }
